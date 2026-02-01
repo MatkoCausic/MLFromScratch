6e7e999463d87ff922e7ec799c4aeea6d54b0490
MatkoCausic <matko.causic@gmail.com>
2026-02-01 23:07:31 +0100

    Added Run() function and functionality

--- Machine Learning/LinearRegression_Algorithm.cs
+++ Machine Learning/LinearRegression_Algorithm.cs
@@ -6,6 +6,32 @@ namespace Machine_Learning
 {
     internal class LinearRegression_Algorithm : IAlgorithm
     {
+        private double[][] outputFunction;
+        public double[][] OutputFunction
+        {
+            get { return outputFunction; }
+            private set { outputFunction = value; }
+        }
 
+        public LinearRegression_Algorithm()
+        {
+            outputFunction = new double[][]
+            {
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0]
+            };
+
+        }
+
+        public void Run(double[][] input, double[][] output)
+        {
+            /* buffer = (inputT * input)^-1 * inputT * output */
+            double[][] buffer = Matrix.Inverse(Matrix.Multiply(Matrix.Transpose(input), input));
+            buffer = Matrix.Multiply(buffer, Matrix.Transpose(input));
+            buffer = Matrix.Multiply(buffer, output);
+
+            OutputFunction = buffer;
+        }
     }
 }
