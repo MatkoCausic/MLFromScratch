5657efe603d0fbf818d3312530028f67c321df1e
MatkoCausic <matko.causic@gmail.com>
2026-02-03 18:00:18 +0100

    Added extra class and utility

---
+++ Machine Learning/LogisticRegression_Algorithm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Machine_Learning
+{
+    internal class LogisticRegression_Algorithm
+    {
+        public LogisticRegression_Algorithm()
+        {
+
+        }
+
+
+    }
+}
---
+++ Machine Learning/StatisticalCalculations.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Machine_Learning
+{
+    internal static class Statistic
+    {
+
+    }
+}
