70040c0ac8f63e4bc495ec063ea9a885b91196d5
MatkoCausic <matko.causic@gmail.com>
2026-02-09 11:01:19 +0100

    Adjusted the Jenkinsfile, added C# script for building options

---
+++ BuildTool/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+static int Run(string file, string args, string workingDir)
+{
+    var psi = new ProcessStartInfo(file, args)
+    {
+        WorkingDirectory = workingDir,
+        RedirectStandardOutput = true,
+        RedirectStandardError = true,
+        UseShellExecute = false
+    };
+
+    using var p = Process.Start(psi)!;
+    p.OutputDataReceived += (_, e) =>
+    {
+        if (e.Data != null)
+            Console.WriteLine(e.Data);
+    };
+    p.ErrorDataReceived += (_, e) =>
+    {
+        if (e.Data != null)
+            Console.Error.WriteLine(e.Data);
+    };
+    p.BeginOutputReadLine();
+    p.BeginErrorReadLine();
+    p.WaitForExit();
+    return p.ExitCode;
+}
+
+var repoRoot = Directory.GetCurrentDirectory();
+
+var csproj = Path.Combine(repoRoot, "MachineLearning", "Machine Learning.csproj");
+
+var outDir = Path.Combine(
+    Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory),
+    "Builds", "Win"
+);
+
+Directory.CreateDirectory(outDir);
+
+var args =
+    $"publish \"{csproj}\" -c Release -r win-x64 --self-contained true " +
+    $"-o \"{outDir}\" " +
+    "/p:PublishSingleFile=true " +
+    "/p:IncludeNativeLibrariesForSelfExtract=true";
+
+Console.WriteLine($"Publishing to: {outDir}");
+return Run("dotnet", args, repoRoot);
--- Jenkinsfile
+++ Jenkinsfile
@@ -16,16 +16,7 @@ pipeline {
     stage('Publish (Windows)') {
       steps {
         script {
-          if (isUnix()) {
-            error("Ovaj job je trenutno podešen samo za Windows (bat).")
-          }
-
-          def outDir = "${env.USERPROFILE}\\Desktop\\Builds\\Win"
-
-          bat "if not exist \"${outDir}\" mkdir \"${outDir}\""
-          bat "dotnet --info"
-          bat "dotnet publish \"Machine Learning.slnx\" -c Release -o \"${outDir}\""
-          bat "dir \"${outDir}\""
+          bat 'dotnet run --project BuildTool -c Release'
         }
       }
     }
