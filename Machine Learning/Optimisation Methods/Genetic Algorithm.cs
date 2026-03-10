0de14e0bda04f47abbe6dfdb7ae518eb5f95a3c9
MatkoCausic <matko.causic@gmail.com>
2026-03-10 12:26:35 +0100

    Added new ToDo stuff, added high-level works of genetic algorithm

---
+++ Machine Learning/Optimisation Methods/Genetic Algorithm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Machine_Learning.Optimisation_Methods
+{
+    internal class Genetic_Algorithm
+    {
+        int populationSize;
+        int numberOfGenerations;
+        float crossoverProbability;
+        float mutationProbability;
+        float mutation_deviation;
+
+        public Genetic_Algorithm(int populationSize, int numberOfGenerations, float crossoverProbability, float mutationProbability, float mutation_deviation)
+        {
+            this.populationSize = populationSize;
+            this.numberOfGenerations = numberOfGenerations;
+            this.crossoverProbability = crossoverProbability;
+            this.mutationProbability = mutationProbability;
+            this.mutation_deviation = mutation_deviation;
+        }
+        
+        public void Execute()
+        {
+            GeneratePopulation();
+            FitnessFunction();
+            if (!TerminateCondition())
+            {
+                Selection();
+                Crossover();
+                Mutation();
+            }
+        }
+
+        double[][] GeneratePopulation()
+        {
+
+        }
+        void Selection();
+        void Crossover();
+        void Mutation();
+        float FitnessFunction();
+        bool TerminateCondition();
+    }
+}
